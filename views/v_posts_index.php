09d68d779ba6043696654065ccd9922d4e66efc1
Andrew Wong <awong@fas.harvard.edu>
2013-11-05 19:16:39 -0500

    Editing v_posts_index and c_posts

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -11,8 +11,8 @@
          </span>
 
        <!--If the user id  matches with the user who made the post, then provide the option of editing or deleting entry -->
-        <?php if($user->user_id == $post[post_user_id]): ?>                
-         <a href=/posts/edit<?=$post['post_id']?>>Edit</a> / 
+        <?php if($user->user_id == $post['post_user_id']): ?>                
+         <a href=/posts/edit<?=$post['post_id']?>>Edit</a> - 
          <a href=/posts/delete<?=$post['post_id']?>>Delete</a>        
         
         <?php endif; ?>                         
